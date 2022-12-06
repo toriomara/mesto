ce4065668e5d4f61b6a541df03848eb85483a682
toriomara <budkevich.vladimir@gmail.com>
2022-12-06 10:08:18 +0300

    fix: after firstt review

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,7 +1,7 @@
 export const initialCards = [
   {
     title: 'Карачаево-Черкесская Республика',
-    link: 'https://image1s.unsplash.com/photo-1627329904799-607897b1eb60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    link: 'https://images.unsplash.com/photo-1627329904799-607897b1eb60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
   },
   {
     title: 'Гора Эльбрус',
